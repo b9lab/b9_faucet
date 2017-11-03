bbbee0bb43f9c2cf399375378480d618c23f5769
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-11-03 17:17:19 +0000

    Added ether-gas-reporter.

---
+++ .ethgas.js
@@ -0,0 +1,4 @@
+module.exports = {
+  currency: "GBP",    // Default: "EUR" (loaded at run-time from the `coinmarketcap` api)
+  gasPrice: 20 * 1e9  // Default: (~5 gwei, loaded at run-time from the `blockcypher` api)
+}
--- truffle.js
+++ truffle.js
@@ -13,5 +13,8 @@ module.exports = {
       port: 8545,
       network_id: '3'
     }
+  },
+  mocha: {
+    reporter: 'eth-gas-reporter'
   }
 }
