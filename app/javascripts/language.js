ce579976c4e10a76f5c8e17764a89b5923c7d38b
Sam Schmeink <sam@b9lab.com>
2018-10-17 16:23:14 +0200

    language is now pre selected by browser agent

--- app/javascripts/language.js
+++ app/javascripts/language.js
@@ -5,18 +5,27 @@ LanguageSelection.prototype.languageData = {};
 
 LanguageSelection.prototype.init = function() {
     var ctx = this;
-    var urlParams = new URLSearchParams(window.location.search);
-    this.selectedLanguage = urlParams.has("lang") ? urlParams.getAll("lang") : "en";
 
-    fetch("js/language.json").then(function(response) {
+    return fetch("js/language.json").then(function(response) {
         return response.json();
     }).then(function(json) {
         ctx.languageData = json;
+        let browserLang = navigator.language.substr(0, 2);
+        ctx.selectedLanguage = ctx.isLanguageAvailable(browserLang) ? browserLang : "en";
+
+        var urlParams = new URLSearchParams(window.location.search);
+        ctx.selectedLanguage = urlParams.has("lang") ? urlParams.getAll("lang") : ctx.selectedLanguage;
+        return Promise.resolve();
     });
 }
 
 LanguageSelection.prototype.getTranslatedString = function(id) {
     return this.languageData["languages"][this.selectedLanguage][id];
 }
+
+LanguageSelection.prototype.isLanguageAvailable = function(lang) {
+    return typeof(this.languageData["languages"][lang]) != "undefined";
+}
+
 
 var languageSelection = new LanguageSelection();
