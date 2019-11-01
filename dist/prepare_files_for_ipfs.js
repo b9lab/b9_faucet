1113a220dcdcf0bae3d9cfcb815086d32988a1cc
Xavier Lepretre <xavier.lepretre@gmail.com>
2019-11-01 12:34:35 +0000

    Omission.

--- dist/prepare_files_for_ipfs.js
+++ dist/prepare_files_for_ipfs.js
@@ -7,6 +7,7 @@ const path = require("path");
     { path: "../build/css", flags: "-Rv" },
     { path: "../build/images", flags: "-Rv" },
     { path: "../build/js", flags: "-Rv" },
+    { path: "../build/statuspage", flags: "-Rv" },
     { path: "../build/index.html", flags: "-v" }
 ].forEach(element => {
     console.log(execSync(`cp ${element.flags} ${path.resolve(__dirname, element.path)} ${__dirname}/ipfs`).toString());
